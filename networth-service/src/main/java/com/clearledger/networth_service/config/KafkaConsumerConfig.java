d34fb2e600b0aed137bdfd369078392d730e6244
Bhagyesh2003 <chaudharibhagyesh21@gmail.com>
2026-08-19 20:43:54 +0530

    Updated kafka config

--- networth-service/src/main/java/com/clearledger/networth_service/config/KafkaConsumerConfig.java
+++ networth-service/src/main/java/com/clearledger/networth_service/config/KafkaConsumerConfig.java
@@ -2,6 +2,8 @@ package com.clearledger.networth_service.config;
 
 import com.clearledger.networth_service.event.ExpenseLoggedEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.CommonClientConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,29 +22,74 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String securityProtocol;
+
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    private String saslMechanism;
+
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String saslJaasConfig;
+
     @Bean
     public ConsumerFactory<String, ExpenseLoggedEvent> consumerFactory() {
+
         JsonDeserializer<ExpenseLoggedEvent> deserializer =
                 new JsonDeserializer<>(ExpenseLoggedEvent.class);
+
         deserializer.setRemoveTypeHeaders(false);
         deserializer.addTrustedPackages("*");
         deserializer.setUseTypeMapperForKey(true);
 
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "networth-consumer-group");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        return new DefaultKafkaConsumerFactory<>(props,
-                new StringDeserializer(), deserializer);
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers
+        );
+
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "networth-consumer-group"
+        );
+
+        props.put(
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                "earliest"
+        );
+
+        // Kafka security
+        props.put(
+                CommonClientConfigs.SECURITY_PROTOCOL_CONFIG,
+                securityProtocol
+        );
+
+        props.put(
+                SaslConfigs.SASL_MECHANISM,
+                saslMechanism
+        );
+
+        props.put(
+                SaslConfigs.SASL_JAAS_CONFIG,
+                saslJaasConfig
+        );
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, ExpenseLoggedEvent>
     kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, ExpenseLoggedEvent> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+
+        ConcurrentKafkaListenerContainerFactory<String, ExpenseLoggedEvent>
+                factory = new ConcurrentKafkaListenerContainerFactory<>();
+
         factory.setConsumerFactory(consumerFactory());
+
         return factory;
     }
 }
